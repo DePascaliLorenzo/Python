0bf54b0af02048fa73a52c541037a945f3b7e9f4
Lorenzo De Pascali <DePascali.L97@gmail.com>
2025-03-05 09:56:04 +0100

    Aggiunti cicli con stampa di soli elementi con indice pari, cicli inversi, break e continue

--- iterativi/ciclo_for.py
+++ iterativi/ciclo_for.py
@@ -15,3 +15,44 @@ print ('\n')
 lista = ['uno', 'due', 'tre', 'quattro']
 for elemento in lista:
     print(elemento)
+
+print("\n")
+
+# iteriamo gli elementi di una lista per stampare solo quelli con indice pari
+for indice in range(0, len(lista), 2):
+    print(lista[indice])
+
+print('\n')
+
+# iteriamo gli elementi di una lista dall'ultimo al primo
+for indice in range(len(lista)-1, -1, -1):
+    print(lista[indice])
+
+print('\n')
+
+# iteriamo gli elementi di una lista dall'ultimo al primo (iterazione inversa) con slicing lista
+for elemento in lista[::-1]:
+    print(elemento)
+
+print('\n')
+
+# abbiamo una lista di numeri e li vogliamo stampare ma se nella lista c'è il numero 5 fermiamo il ciclo
+numeri = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+for numero in numeri:
+    if numero == 5:
+        break
+    print(numero)
+else:
+    print("Il ciclo ha terminato il suo lavoro")
+
+print('\n')
+
+# abbiamo una lista di numeri e li vogliamo stampare ma se nella lista c'è il numero 5 saltiamo la stampa di quel numero
+for numero in numeri:
+    if numero == 5:
+        continue
+    print(numero)
+else:
+    print("Il ciclo ha terminato il suo lavoro")
+
+print("Il programma è finito")
