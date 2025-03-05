e0042f6b5c3d5c733474b76c26fd09478522331c
DePascaliLorenzo <depascali.l97@gmail.com>
2025-03-05 13:58:04 +0100

    Aggiunto esercizio 6

---
+++ esercizi_02/esercizio_06.py
@@ -0,0 +1,19 @@
+"""
+I. Crea uno script nominandolo “esercizio_06”
+II. Utilizzando dei costrutti iterativi, crea un algoritmo per produrre l'output richiesto
+OUTPUT Stampa in console della seguente rappresentazione:
+X O X O X O X O X O
+X O X O X O X O X O
+X O X O X O X O X O
+X O X O X O X O X O
+X O X O X O X O X O
+X O X O X O X O X O
+X O X O X O X O X O
+X O X O X O X O X O
+X O X O X O X O X O
+X O X O X O X O X O
+"""
+
+# Utilizzo di un costrutto iterativo per produrre l'output richiesto
+for _ in range(10):
+    print("X O " * 5)
