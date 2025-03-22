11d662b8dc9e689d9db320537966e030e5c456de
Lorenzo De Pascali <DePascali.L97@gmail.com>
2025-03-22 22:41:24 +0100

    Continuo esercizio analisi reddito

--- esercizi_06/analisi_reddito.py
+++ esercizi_06/analisi_reddito.py
@@ -12,23 +12,18 @@ import matplotlib.pyplot as plt
 _DATASET_PATH = '06. people.csv'
 _DATASET_PATH_FIXED = '06.people_sistemato.csv'
 label_old = ['age','workclass','fnlwgt','education','education-num','marital-status','occupation','relationship','race','sex','capital-gain','capital-loss','hours-per-week','native-country','target']
-label_new = ['Età', 'Categoria di Lavoro','Peso Campionario','Educazione','Anni di Studio','Stato Coniugale','Mansione','Relazione','Razza','Sesso','Entrate','Uscite','Ore Settimanali','Paese d\'Origine','Obiettivo']
+label_new = ['Età', 'Categoria di Lavoro','Peso Campionario','Educazione','Anni di Studio','Stato Coniugale','Mansione',
+            'Relazione','Razza','Sesso','Entrate','Uscite','Ore Settimanali','Paese d\'Origine','Obiettivo']
 drop_list_old = [label_old[2], label_old[4], label_old[14]]
-drop_list_new = [label_new[2], label_new[4], label_new[14]]
-native_countries_list = ['?', 'United-States', 'Cuba', 'Jamaica', 'India', 'Mexico', 'South', 'Puerto-Rico', 'Honduras',
-                         'England', 'Canada', 'Germany', 'Iran', 'Philippines', 'Italy', 'Poland', 'Columbia', 'Cambodia',
-                         'Thailand', 'Ecuador', 'Laos', 'Taiwan', 'Haiti', 'Portugal', 'Dominican-Republic', 'El-Salvador',
-                         'France', 'Guatemala', 'China', 'Japan', 'Yugoslavia', 'Peru', 'Outlying-US(Guam-USVI-etc)',
-                         'Scotland', 'Trinadad&Tobago', 'Greece', 'Nicaragua', 'Vietnam', 'Hong', 'Ireland', 'Hungary',
-                         'Holand-Netherlands']
+work_class_list = ['State-gov', 'Self-emp-not-inc', 'Private', 'Federal-gov', 'Local-gov', '?', 'Self-emp-inc', 'Without-pay', 'Never-worked',]
 
 class ModelloReddito(ModelloBase):
 
     def __init__(self, dataset_path):
         self.dataframe = pd.read_csv(dataset_path)
         self.dataframe_sistemato = self.sistemazione_dataframe()
-        # self.contingenza_classe = self.tabella_contingenza('Classe Passeggero', 'Sopravvissuto')
-        #self.contingenza_genere = self.tabella_contingenza('Genere', 'Sopravvissuto')
+        self.contingenza_razza = self.tabella_contingenza(label_old[8], label_old[10])
+        # self.contingenza_genere = self.tabella_contingenza('Genere', 'Sopravvissuto')
         # self.correlazione_spearman('Età','Sopravvissuto')
         # self.grafici_contingenza()
         # self.grafico_spearman()
@@ -36,8 +31,7 @@ class ModelloReddito(ModelloBase):
 
     def sistemazione_dataframe(self):
 
-        df_sistemato = self.dataframe.drop(drop_list_old, axis = 1)
-
+        df_sistemato = self.dataframe
         df_sistemato = df_sistemato.rename(columns = {
             label_old[0] : label_new[0],
             label_old[1] : label_new[1],
@@ -52,15 +46,20 @@ class ModelloReddito(ModelloBase):
             label_old[12] : label_new[12],
             label_old[13] : label_new[13],
         })
+        """
+        # assegnazione valori numerici alla colonna Categoria di Lavoro
+        df_sistemato[label_new[1]] = df_sistemato[label_new[1]].map({work_class_list[0]: '7', work_class_list[1]: '6',
+                                                                     work_class_list[2]: '5', work_class_list[3]: '4',
+                                                                     work_class_list[4]: '3', work_class_list[5]: None,
+                                                                     work_class_list[6]: '2', work_class_list[7]: '1', work_class_list[8]: '0'})
 
+        # assegnazione valori numerici alla colonna Mansione
+        df_sistemato[label_new[6]] = df_sistemato[label_new[6]].map({'Priv-house-serv': '0', 'Handlers-cleaners': '1',
+                                        'Farming-fishing': '2', 'Other-service': '3', 'Machine-op-inspct': '4', 'Transport-moving': '5',
+                                        'Craft-repair': '6', 'Sales': '7', 'Tech-support': '8', 'Adm-clerical': '9', 'Protective-serv': '10',
+                                        'Exec-managerial': '11', 'Prof-specialty': '12', 'Armed-Forces': '13', '?': None})
+        """
         return df_sistemato
-        """
-        df_sistemato[label_new[13]] = df_sistemato[label_new[13]].map({'?':0, 'United-States':1, 'Cuba':2, 'Jamaica':3, 'India':4, 'Mexico':5, 'South':6, 'Puerto-Rico':7, 'Honduras':8, 'England':9,
-'Canada':10, 'Germany':11, 'Iran':12, 'Philippines':13, 'Italy':14, 'Poland':15, 'Columbia':16, 'Cambodia':17, 'Thailand':18, 'Ecuador':19,
-'Laos':20, 'Taiwan':21, 'Haiti':22, 'Portugal':23, 'Dominican-Republic':24, 'El-Salvador':25, 'France':26, 'Guatemala':27, 'China':28, 'Japan':29,
-'Yugoslavia':30, 'Peru':31, 'Outlying-US(Guam-USVI-etc)':32, 'Scotland':33, 'Trinadad&Tobago':34, 'Greece':35, 'Nicaragua':36, 'Vietnam':37, 'Hong':38,
-'Ireland':39, 'Hungary':40, 'Holand-Netherlands':41})
-        """
 
     def tabella_contingenza(self,column, target):
         tabella_contingenza = pd.crosstab(self.dataframe[column], self.dataframe[target])
@@ -82,8 +81,7 @@ class ModelloReddito(ModelloBase):
 
         return tabella_contingenza
 
-
 modello = ModelloReddito(_DATASET_PATH)
-modello.analisi_generali(modello.dataframe_sistemato)
-modello.dataframe_sistemato.to_csv(_DATASET_PATH_FIXED, index = False)
-modello.analisi_valori_univoci(modello.dataframe_sistemato)
+#modello.analisi_generali(modello.dataframe_sistemato)
+#modello.dataframe_sistemato.to_csv(_DATASET_PATH_FIXED, index = False)
+#modello.analisi_valori_univoci(modello.dataframe_sistemato)
