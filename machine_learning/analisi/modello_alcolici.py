59e598d1660aca87772babbb732fc083ac28b856
Lorenzo De Pascali <DePascali.L97@gmail.com>
2025-03-26 12:55:27 +0100

    Termine modello heart e nuovo esempio sul modello alcolici

---
+++ machine_learning/analisi/modello_alcolici.py
@@ -0,0 +1,68 @@
+from modello_base import ModelloBase
+import pandas as pd
+from scipy.stats import chi2_contingency
+import numpy as np
+
+class ModelloAlcolici(ModelloBase):
+
+    def __init__(self, dataset_path):
+        self.dataframe = pd.read_csv(dataset_path)
+        self.dataframe_sistemato = self.sistemazione_dataframe()
+        self.tabella_contingenza('AGE')
+        self.tabella_contingenza('SEX')
+
+    def sistemazione_dataframe(self):
+
+        # 1. copia dataframe
+        df_sistemato = self.dataframe.copy()
+
+        # 2. drop colonne completamente prive di valori
+        df_sistemato = df_sistemato.dropna(axis = 1, how = 'all')
+
+        # 3. drop colonne con valore unico
+        colonne_unico_valore = []
+        for col in df_sistemato.columns:
+            if df_sistemato[col].nunique() < 2:
+                colonne_unico_valore.append(col)
+        df_sistemato = df_sistemato.drop(colonne_unico_valore, axis = 1)
+
+        # 4. eliminazione righe con valori MEASURE = HSC - SEX = 9 - AGE = Y_GE11
+        indici_righe_da_eliminare = df_sistemato.index[(df_sistemato['MEASURE'] == 'HSC') | (df_sistemato['SEX'] == 9) | (df_sistemato['AGE'] == 'Y_GE11')]
+        df_sistemato = df_sistemato.drop(indici_righe_da_eliminare, axis = 0)
+
+        return df_sistemato
+
+    # metodo per ottenere tabelle di contingenza - test chi-quadro - indici Cramer
+    def tabella_contingenza(self, column):
+
+        # generazione e stampa tabella contingenza
+        tabella_contingenza = pd.crosstab(self.dataframe_sistemato[column],
+                                          self.dataframe_sistemato['DATA_TYPE'],
+                                          values = self.dataframe_sistemato['OBS_VALUE'],
+                                          aggfunc = 'sum')
+
+        # sostituzione label tabella di contingenza
+        tabella_contingenza.columns = tabella_contingenza.columns.map({'11_ALCF_1VOL': 'Cons_Alc_F_Pasto -1/Settimana',
+                                                                       '11_ALC_FUORI': 'Cons_Alc_F_Pasto',
+                                                                       '11_ALC_NFUORI' : 'No_Cons_Alc_F_Pasto'})
+        if column == 'SEX':
+            tabella_contingenza.index = tabella_contingenza.index.map({1: 'Maschi',2:'Femmine'})
+
+        # stampa tabella contingenza
+        print(f'***** TABELLA CONTINGENZA {column} - DATA_TYPE', tabella_contingenza.to_string(), sep='\n')
+
+        # test del chi-quadro e stampa esito (p-value)
+        chi2, p, dof, expected = chi2_contingency(tabella_contingenza)
+        print(f'Il p-value risultante da test del chi-quadro tra {column} - DATA_TYPE è {format(p, '.53f')}')
+
+        # calcolo dell'indice di Cramer e stampa esito
+        totale_osservazioni = tabella_contingenza.sum().sum()
+        dimensione_minima = min(tabella_contingenza.shape) - 1 # valore minimo in una tupla righe-colonne tabella contingenza
+        cramer = np.sqrt(chi2 / (totale_osservazioni * dimensione_minima))
+        print(f'L\'indice di Cramer calcolato sulla tabella di contingenza {column} - DATA_TYPE è: ', cramer)
+
+
+# utilizzo modello
+modello = ModelloAlcolici('../dataset/data_08.csv')
+# modello.analisi_generali(modello.dataframe_sistemato)
+# modello.analisi_valori_univoci(modello.dataframe_sistemato)
--- machine_learning/analisi/modello_heart.py
+++ machine_learning/analisi/modello_heart.py
@@ -1,7 +1,14 @@
+from scipy.cluster.vq import kmeans
+from scipy.stats import alpha
+
 from modello_base import ModelloBase
 import pandas as pd
 from sklearn.preprocessing import StandardScaler, OneHotEncoder
 from sklearn.decomposition import PCA
+from kneed import KneeLocator
+import matplotlib.pyplot as plt
+from sklearn.cluster import KMeans
+import seaborn as sns
 
 _DATASET_PATH = '../dataset/data_07.csv'
 
@@ -12,6 +19,10 @@ class ModelloHeart(ModelloBase):
         self.dataframe = pd.read_csv(dataset_path)
         self.variabili_quantitative, self.variabili_categoriali, self.scaler, self.encoder, self.dataframe_sistemato = self.sistemazione_dataframe()
         self.pca, self.dataframe_ridotto = self.riduzione_dataframe()
+        self.numero_cluster = self.individuazione_cluster()
+        self.livelli_rischio ={0: 'Basso', 1:'Medio', 2: 'Alto'}
+        self.modello_clustering = self.clustering()
+        self.grafico_cluster()
 
     # metodo di sistemazione del dataframe
     def sistemazione_dataframe(self):
@@ -37,10 +48,106 @@ class ModelloHeart(ModelloBase):
         df_ridotto = pd.DataFrame(pca.fit_transform(self.dataframe_sistemato),columns = ['PC1','PC2'])
         return pca, df_ridotto
 
+    # metodo per individuazione numero ottimale cluster
+    def individuazione_cluster(self):
+        inertia = []
+        cluster_range = range(1,11)
+        for k in cluster_range:
+            kmeans = KMeans(n_clusters=k, random_state=42)
+            kmeans.fit(self.dataframe_ridotto)
+            inertia.append(kmeans.inertia_)
+        # analisi numero ottimale cluster con strumento automatico
+        knee_locator = KneeLocator(cluster_range, inertia, curve = 'convex', direction='decreasing')
+        numero_ottimale_cluster = knee_locator.knee
+        print(f'Numero ottimale cluster rilevato da KneeLocator: {numero_ottimale_cluster}')
+        # conferma del numero ottimale del cluster mediante "grafico a ginocchio"
+        plt.figure(figsize=(10,6))
+        plt.plot(cluster_range,inertia, marker = 'o')
+        plt.xlabel('Numero Cluster')
+        plt.ylabel('Inertia')
+        plt.title('Metodo Elbow per selezione numero ottimale cluster')
+        plt.show()
+
+        return numero_ottimale_cluster
+
+    # metodo per il clustering delle nostre osservazioni
+    def clustering(self):
+        # creazione e addestramento del modello (aggiunta al dataframe di una nuova colonna con n° del cluster assegnato)
+        kmeans = KMeans(n_clusters=self.numero_cluster, random_state=42)
+        self.dataframe_ridotto['Cluster'] = kmeans.fit_predict(self.dataframe_ridotto)
+        # aggiunta nuova colonna al dataframe per sostituzione n° cluster con etichetta testuale
+        self.dataframe_ridotto['Rischio'] = self.dataframe_ridotto['Cluster'].map(self.livelli_rischio)
+        # conversione colonna Rischio in una colonna di tipo categorico ordinato
+        ordine_rischio = [self.livelli_rischio[key] for key in sorted(self.livelli_rischio.keys())]
+        self.dataframe_ridotto['Rischio'] = pd.Categorical(self.dataframe_ridotto['Rischio'], categories = ordine_rischio, ordered=True)
+
+        return kmeans
+
+    # metodo per visualizzazione dei cluster in un grafico
+    def grafico_cluster(self, paziente = None):
+        plt.figure(figsize=(14,10))
+        # visualizzazione osservazioni
+        sns.scatterplot(data=self.dataframe_ridotto,x = 'PC1', y = 'PC2', hue = 'Rischio', palette='viridis', style='Rischio', s = 100, alpha = 0.7)
+        # visualizzazione centroidi
+        centroidi = self.modello_clustering.cluster_centers_
+        plt.scatter(centroidi[:,0], centroidi[:,1], c='black',marker = 'o', s = 300, label = 'Centroidi')
+        # visualizzazione nuovo paziente se esiste
+        if not paziente is None:
+            plt.scatter(paziente['PC1'], paziente['PC2'], c = 'red', s=200, marker = 'X', label = 'Nuovo Paziente')
+        # indicazioni testuali
+        plt.title('Distribuzione pazienti per livelli di rischio cardiaco')
+        plt.xlabel('PC1')
+        plt.ylabel('PC2')
+        plt.legend(title = 'Livelli di Rischio')
+        plt.show()
+
+    # la distanza tra i centroidi indica la bontà del modello, più sono lontani, meglio è
+
+    # metodo per la valutazione livello di rischio di un nuovo paziente
+    def valutazione_paziente(self, dati_paziente):
+        # costruzione dataframe per il nuovo paziente
+        df_paziente = pd.DataFrame([dati_paziente]) # singola osservazione
+        # standardizzazione variabili quantitative
+        df_paziente_quant = pd.DataFrame(self.scaler.transform(df_paziente[self.variabili_quantitative]), columns=self.variabili_quantitative)
+        # one-hot-encoding variabili categoriali
+        df_paziente_cat = pd.DataFrame(self.encoder.transform(df_paziente[self.variabili_categoriali]))
+        df_paziente_cat.columns = self.encoder.get_feature_names_out(self.variabili_categoriali)
+        # riunificazione dataframe
+        df_paziente_sistemato = pd.concat([df_paziente_quant,df_paziente_cat], axis = 1)
+        # riduzione dimensionale
+        df_paziente_ridotto = pd.DataFrame(self.pca.transform(df_paziente_sistemato), columns = ['PC1','PC2'])
+        # predizione del cluster
+        cluster = self.modello_clustering.predict(df_paziente_ridotto)[0]
+        livello_rischio = self.livelli_rischio[cluster]
+        print(f'Livelli Rischio Paziente: {livello_rischio}')
+        # invocazione metodo per visualizzazione nel grafico
+        self.grafico_cluster(df_paziente_ridotto)
+
+
+
+
 # utilizzo modello
 modello = ModelloHeart(_DATASET_PATH)
-modello.analisi_generali(modello.dataframe_ridotto)
+# modello.analisi_generali(modello.dataframe_ridotto)
 # modello.analisi_valori_univoci(modello.dataframe, ['age','trestbps','chol','thalach','oldpeak'])
 # modello.analisi_indici_statistici(modello.dataframe)
 # modello.individuazione_outliers(modello.dataframe,['sex','cp','fbs','restecg','exang','slope','thal'])
 # se gli outliers superano il 10% cominciano a dare fastidio al dataset e si fa solo per le quantitative
+
+# predizione nuovo paziente
+nuovo_paziente = {
+    "age": 55,
+    "sex": 1,
+    "cp": 2,
+    "trestbps": 130,
+    "chol": 250,
+    "fbs": 0,
+    "restecg": 1,
+    "thalach": 150,
+    "exang": 0,
+    "oldpeak": 1.5,
+    "slope": 1,
+    "ca": 0,
+    "thal": 2
+}
+modello.valutazione_paziente(nuovo_paziente)
